6613844428afe8ee32ce27d8f929898fa3aedd3b
PJDnDTime <35343897+PJDnDTime@users.noreply.github.com>
2018-06-29 15:32:32 -0700

    Add files via upload

---
+++ stream/Webhook-TwitchStreamIsLiveAnnouncementForDiscord.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $("#form").submit(function(e) {
+	e.preventDefault();
+    var d = new Date();
+	var sdata = {
+		"content": "Hey there @here, DnDTime has gone live on twitch! Come check it out!",
+	    "embeds": [{
+		"description": "**["+$("#title").val()+"](https://twitch.tv/dndtime)**\n"+$("#desc").val(),
+		"color": 6570404,
+		"timestamp": d,
+		"footer": {
+			"icon_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/a6d79381f007c412-profile_image-70x70.png",
+			"text": "Started Streaming",
+		},
+		"image": {
+		    "url": "https://static-cdn.jtvnw.net/previews-ttv/live_user_dndtime-1280x720.jpg"
+		}
+	    }]
+	};
+	$.ajax({
+	    url: $("#webhook").val(),
+	    type: 'POST',
+	    data: JSON.stringify(sdata),
+	    contentType: "application/json"
+	})
+    });
+});
