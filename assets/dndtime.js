0dbd737cfa97bd30231272cee7b0b744079c3611
PJDnDTime <35343897+PJDnDTime@users.noreply.github.com>
2018-10-16 20:24:16 -0700

    Update dndtime.js

--- assets/dndtime.js
+++ assets/dndtime.js
@@ -14,7 +14,7 @@ function loadContent() {
 }
 
 // Check for changed anchor navigation
-window.addEventListener( "hashchange", loadContent );
+window.onhashchange = loadContent;
 
 // Navigation bar responsive scaling
 function navFunction() {
